320c795e1a5dd2ff839b5926bd77863723b8d212
flavabasempsf-png <flavabasempsf@gmail.com>
2026-07-16 15:53:32 +0100

    feat: add router foundation

---
+++ os/js/router.js
@@ -0,0 +1,24 @@
+/*
+==========================================
+FLAVA OS
+Version 1.0.0
+
+Router
+==========================================
+*/
+
+const Router = {
+
+    init() {
+
+        console.log("Router initialised.");
+
+    }
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    Router.init();
+
+});
