8e3a79cc1988483df3c63ac536b7c7eb21cd35a5
flavabasempsf-png <flavabasempsf@gmail.com>
2026-07-16 23:54:33 +0100

    feat: add guest database foundation
    Create the central guest data store for Event Control and future modules.

---
+++ os/data/guests.js
@@ -0,0 +1,33 @@
+/*
+==========================================
+FLAVA OS
+Guest Database
+Version 1.0.0
+==========================================
+*/
+
+const GuestDatabase = {
+
+    event: {
+        name: "Flava Base Launch Party",
+        date: "18 July 2026",
+        venue: "Molesworth Village Hall"
+    },
+
+    guests: [
+
+        {
+            id: 1,
+            group: "Scott Group",
+            name: "Steve Scott",
+            tickets: 1,
+            starter: "Lamb Samosas",
+            main: "Punjabi Chicken Curry & Rice",
+            drink: "Mango Lassi",
+            checkedIn: false,
+            ticketNumber: ""
+        }
+
+    ]
+
+};
